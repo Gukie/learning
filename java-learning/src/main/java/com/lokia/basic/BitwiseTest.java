f09171e822db6531c2f47a13ec75d63ba8f34736
wb-gs313456 <wb-gs313456@alibaba-inc.com>
2018-09-07 16:03:31 +0800

    >> ,>>> the difference

--- java-learning/src/main/java/com/lokia/basic/BitwiseTest.java
+++ java-learning/src/main/java/com/lokia/basic/BitwiseTest.java
@@ -7,11 +7,28 @@ package com.lokia.basic;
 public class BitwiseTest {
 
     public static void main(String[] args) {
-        int a = 8;
-        int right = a>>1;
-        int left = a<<0;
 
-        System.out.println("after left bitwise:"+left);
-        System.out.println("after right bitwise:"+right);
+        System.out.println("10: "+Integer.toBinaryString(10));
+        System.out.println("10>>2: "+(10>>2)); // 除于 2的2次方
+        System.out.println("10>>>2: "+(10>>>2));
+        System.out.println(Integer.toBinaryString(10>>2));
+        System.out.println(Integer.toBinaryString(10>>>2));
+
+        System.out.println();
+        System.out.println("-15: "+Integer.toBinaryString(-15));
+        System.out.println("-15>>2: "+(-15>>2));
+        System.out.println("-15>>>2: "+( -15 >>> 1) ); // 高位会补零，得到的可能会是一个正数
+        System.out.println(Integer.toBinaryString(-15>>2));
+        System.out.println(Integer.toBinaryString(-15>>>1));
+
+        System.out.println();
+        System.out.println(Integer.toBinaryString(4));
+        System.out.println("4<<2: "+(4<<2)); // 乘以2的2次方
+        System.out.println("-4<<2: "+(-4<<2));
+        System.out.println(Integer.toBinaryString(4<<2));
+        System.out.println(Integer.toBinaryString(-4<<2));
     }
+
+
+
 }

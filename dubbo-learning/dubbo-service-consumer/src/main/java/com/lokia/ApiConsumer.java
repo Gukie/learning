ff57c802f58f61b16803040089457ae16caa7dbc
lokia.gu <zjut.gu@gmail.com>
2018-10-18 23:01:18 +0800

    this version works. 1. use zookeeper://127.0.0.1:2181 as the registry address 2. use zkclient as registry client

--- dubbo-learning/dubbo-service-consumer/src/main/java/com/lokia/ApiConsumer.java
+++ dubbo-learning/dubbo-service-consumer/src/main/java/com/lokia/ApiConsumer.java
@@ -1,9 +1,9 @@
 package com.lokia;
 
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.ReferenceConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
 import com.lokia.service.UserService;
-import org.apache.dubbo.config.ApplicationConfig;
-import org.apache.dubbo.config.ReferenceConfig;
-import org.apache.dubbo.config.RegistryConfig;
 
 public class ApiConsumer {
     public static void main(String[] args) {
@@ -12,7 +12,8 @@ public class ApiConsumer {
         applicationConfig.setName("dubbo-service-consumer");
 
         RegistryConfig registryConfig = new RegistryConfig();
-        registryConfig.setAddress("localhost:9090");
+        registryConfig.setAddress("zookeeper://127.0.0.1:2181");
+        registryConfig.setClient("zkclient");
 
         ReferenceConfig referenceConfig = new ReferenceConfig();
         referenceConfig.setInterface(UserService.class);
@@ -22,8 +23,8 @@ public class ApiConsumer {
         referenceConfig.setVersion("1.0.0");
 
 
-        Object result = referenceConfig.get();
-        System.out.println(result);
+        UserService user = (UserService) referenceConfig.get();
+        System.out.println(user.getName());
 
     }
 }
--- dubbo-learning/dubbo-service-producer/src/main/java/com/lokia/main/ApiProducer.java
+++ dubbo-learning/dubbo-service-producer/src/main/java/com/lokia/main/ApiProducer.java
@@ -1,12 +1,10 @@
 package com.lokia.main;
 
+import com.alibaba.dubbo.config.ApplicationConfig;
+import com.alibaba.dubbo.config.RegistryConfig;
+import com.alibaba.dubbo.config.ServiceConfig;
 import com.lokia.service.UserService;
 import com.lokia.service.impl.UserServiceImpl;
-import org.apache.dubbo.config.ApplicationConfig;
-import org.apache.dubbo.config.ProtocolConfig;
-import org.apache.dubbo.config.RegistryConfig;
-import org.apache.dubbo.config.ServiceConfig;
-
 import java.io.IOException;
 
 
@@ -18,26 +16,23 @@ public class ApiProducer {
         applicationConfig.setName("dubbo-service-producer");
 
         RegistryConfig registryConfig = new RegistryConfig();
-        registryConfig.setAddress("localhost");
-        registryConfig.setPort(2181);
+        registryConfig.setAddress("zookeeper://127.0.0.1:2181");
         registryConfig.setClient("zkclient");
 
 
-        ProtocolConfig protocolConfig = new ProtocolConfig();
-        protocolConfig.setName("zookeeper");
-//        protocolConfig.setPort(2181);
 
         ServiceConfig serviceConfig = new ServiceConfig();
         serviceConfig.setInterface(UserService.class);
         serviceConfig.setRef(new UserServiceImpl());
         serviceConfig.setRegistry(registryConfig);
         serviceConfig.setApplication(applicationConfig);
-        serviceConfig.setProtocol(protocolConfig);
+//        serviceConfig.setProtocol(protocolConfig);
         serviceConfig.setVersion("1.0.0");
         serviceConfig.setGroup("lokia-group");
 
         serviceConfig.export();
 
+        System.out.println("successfully start provider...");
         System.in.read();
     }
 

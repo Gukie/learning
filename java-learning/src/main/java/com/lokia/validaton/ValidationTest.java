374c1af30f40c1eba42ef5761c1b6f829bc55cdc
lokia.gu <zjut.gu@gmail.com>
2018-10-24 23:34:02 +0800

    如果只是一个注解，这也太弱了。。。明天继续寻找答案

--- java-learning/src/main/java/com/lokia/validaton/ValidationTest.java
+++ java-learning/src/main/java/com/lokia/validaton/ValidationTest.java
@@ -1,6 +1,11 @@
 package com.lokia.validaton;
 
+/**
+ * 通过生命@NotNull，然后在某个地方进行统一的校验, 单这也太弱了吧...
+ * refer: https://www.jianshu.com/p/a997d03d0cd6
+ */
 public class ValidationTest {
+
 
     public static void main(String[] args) {
         ValidationBean validationBean = new ValidationBean();

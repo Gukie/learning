3273821953ecd29d9ad9f10ce09a2f16c9608c67
lokia.gu <zjut.gu@gmail.com>
2018-10-25 23:33:42 +0800

    add hibernate validator to provide validator provider. question is: is there any exception thrown when validation fails? should done by application?

--- java-learning/src/main/java/com/lokia/validaton/ValidationTest.java
+++ java-learning/src/main/java/com/lokia/validaton/ValidationTest.java
@@ -1,5 +1,11 @@
 package com.lokia.validaton;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
 /**
  * 通过生命@NotNull，然后在某个地方进行统一的校验, 单这也太弱了吧...
  * refer: https://www.jianshu.com/p/a997d03d0cd6
@@ -8,8 +14,14 @@ public class ValidationTest {
 
 
     public static void main(String[] args) {
+
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+
         ValidationBean validationBean = new ValidationBean();
-        System.out.println(validationBean.getName());
+        Set<ConstraintViolation<ValidationBean>> validateResult =  validator.validate(validationBean);
+        validateResult.forEach(item-> System.out.println(item.getMessage()));
+        System.out.println("***********"+validationBean.getName());
 
         validationBean.testNotNull(null);
     }

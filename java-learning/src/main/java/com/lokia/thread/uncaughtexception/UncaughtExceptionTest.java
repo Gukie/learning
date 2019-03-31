33be46ebd2e5e58dfeb2c8b198e40a93264b9ce5
lokia.gu <zjut.gu@gmail.com>
2019-03-31 10:37:37 +0800

    一般的，在uncaughtException中可以将一些错误信息更加丰富的打印出来； 但是到uncaughtException的时候，基本上该线程的生命周期就已经要结束了，不要在这里进行更多的让线程起死回生的操作，以免扰乱线程的正常生命周期

--- java-learning/src/main/java/com/lokia/thread/uncaughtexception/UncaughtExceptionTest.java
+++ java-learning/src/main/java/com/lokia/thread/uncaughtexception/UncaughtExceptionTest.java
@@ -1,16 +1,46 @@
 package com.lokia.thread.uncaughtexception;
 
+import java.io.IOException;
+import java.util.Optional;
+
 /**
  * @author gushu
  * @data 2018/8/11
  */
 public class UncaughtExceptionTest {
     public static void main(String[] args) {
-        Thread thread = new Thread(new ThreadTest());
+        Thread thread = new Thread(new ThreadTest(),"uncaught-exception-thread");
         thread.setUncaughtExceptionHandler((t, e) -> {
             System.out.println("uncaught exception*****: ");
-            e.printStackTrace();
+            e.printStackTrace(System.out);
+
+            System.out.println(t.getName()+" is still alive or not:"+t.isAlive());
+            try {
+                t.join();
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+            }
         });
         thread.start();
+
+
+        try {
+            Thread.sleep(23);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if(thread.isAlive()){
+            System.out.println(thread.getName()+" is still alive");
+        }else{
+            System.out.println(thread.getName()+" is dead");
+        }
+
+
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
